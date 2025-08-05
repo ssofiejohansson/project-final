83bf9d9918895aef9e75ba3c81c0d6c59e60a9fd
solen80a <sofialennbom@gmail.com>
2025-08-05 13:19:45 +0200

    Created a user model.

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+import crypto from "crypto"
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlenght: 5,
+    maxlenght: 100,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlenght: 3,
+    maxlenght: 100,
+  },
+  accesToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export const user = mongoose.model("User", userSchema)
