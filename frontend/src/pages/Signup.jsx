e73ede35fbed652eb7a719a8228d65d6f3d3924a
oskarnordin <oskarnordin1@gmail.com>
2025-08-05 14:50:48 +0200

    Created simple sign up
    Created simple sign up

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,11 +1,9 @@
-
+import Form from './comp/user/Form';
 
 export const Signup = () => {
   return (
     <>
-      <div className="p-6 text-center">
-        <h1 className="text-3xl font-bold">Signup Page</h1>
-      </div>
+      <Form />
     </>
   );
-}
+};
