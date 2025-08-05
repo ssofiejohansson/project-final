0465c0da3d6fae8db1a270a24bfbd8342760f370
oskarnordin <oskarnordin1@gmail.com>
2025-08-05 14:50:36 +0200

    Created simple login
    Created simple login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,9 @@
-
+import { Userlogin } from './comp/user/Userlogin';
 
 export const Login = () => {
   return (
     <>
-      <div className="p-6 text-center">
-        <h1 className="text-3xl font-bold">Login Page</h1>
-      </div>
+      <Userlogin />
     </>
   );
-}
+};
