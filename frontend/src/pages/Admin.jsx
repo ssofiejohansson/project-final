d69c38d9820f6e866d6d33348d3ffa0886644922
Sofie <sofieannamatilda@hotmail.se>
2025-08-06 14:46:29 +0200

    edit dashboard

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -5,6 +5,10 @@ export const Admin = () => {
     <>
       <div className="p-6 text-center">
         <h1 className="text-3xl font-bold">Admin Page</h1>
+        <p>Welcome XX, here are your subscriptions.</p>
+        <button>Add Subscription</button>
+
+        <button>Log out</button>
       </div>
     </>
   );
