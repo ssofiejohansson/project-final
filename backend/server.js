c86b919b1ea8840262bc64b2b5516240ecf36d34
solen80a <sofialennbom@gmail.com>
2025-08-18 07:44:05 +0200

    Changed endpoint rout to update-reminders

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ app.get('/admin', (req, res) => {
 });
 
 // Route connections
-app.use("/date", updateRemindersRoutes);
+app.use("/update-reminders", updateRemindersRoutes);
 app.use('/users', userRoutes);
 app.use('/subscriptions', subscriptionRoutes);
 app.use('/email', emailRoutes);
