9ed4f7b939aaf53518a19bee5b8e1ae91a23ac61
solen80a <sofialennbom@gmail.com>
2025-08-20 11:05:27 +0200

    finished calculatecost and updated savemoneycontribute json.

--- frontend/src/comp/dashboard/SubscriptionSave.jsx
+++ frontend/src/comp/dashboard/SubscriptionSave.jsx
@@ -1,16 +1,49 @@
-import { Button, Dialog, DialogBody, DialogFooter, DialogHeader } from "@material-tailwind/react";
-import React from "react";
+import { Button, Dialog, DialogBody, DialogFooter, DialogHeader, Tooltip } from "@material-tailwind/react";
 
 import useSubscriptionStore from "../../stores/useSubscriptionStore";
 import { CalculateCost } from "../CalculateCost";
 
 export const SubscriptionSave = () => { 
 
-  const { isSaveOpen, selectedSubSave, closeSaveDialog } = useSubscriptionStore();
+  const { isSaveOpen, closeSaveDialog } = useSubscriptionStore();
+
+  if (!isSaveOpen) return null;
  
   return (
-    <>  
-      <Dialog
+    <> 
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        {/* Backdrop */}
+        <div
+          className="absolute inset-0 bg-black/30"
+          onClick={closeSaveDialog}
+        />
+
+        {/* Popup bubble 1 */}
+        <div className="">
+           {/* Popup bubble 2 */}
+        <div className="relative bg-white rounded-2xl shadow-2xl p-6 max-w-sm w-full z-10 border-2 border-black">
+          
+            <div className="text-lg font-semibold mb-3">Contribute</div>
+
+            <CalculateCost />
+
+            <div className="flex justify-end mt-4">
+              <Button
+                variant="text"
+                color="red"
+                onClick={closeSaveDialog}
+              >
+                Close
+              </Button>
+            </div>
+            <div className="absolute -bottom-4 right-10 w-0 h-0 border-l-8 border-r-8 border-t-8 border-transparent border-t-black" />
+            <div className="absolute -bottom-4 right-10 w-0 h-0">🐝</div>
+            </div>          
+          </div>
+    </div>
+
+
+      {/* <Dialog
         open={isSaveOpen}
         handler={closeSaveDialog}
         animate={{
@@ -19,14 +52,8 @@ export const SubscriptionSave = () => {
         }}
       >
         <DialogHeader>Contribute</DialogHeader>
-        <DialogBody>         
-         <CalculateCost/>
-          <br />
-          {selectedSubSave && (
-            <span className="text-sm text-gray-500">
-              Subscription: {selectedSubSave.name}
-            </span>
-          )}         
+        <DialogBody>                   
+         <CalculateCost/> 
         </DialogBody>
         <DialogFooter>
           <Button
@@ -38,7 +65,7 @@ export const SubscriptionSave = () => {
             <span>Close</span>
           </Button>         
         </DialogFooter>
-      </Dialog>
+      </Dialog> */}
     </>
   );
 }
