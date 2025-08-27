3b353f8188e15451ff8df5b5ac9d6f6b476745d1
Sofie <sofieannamatilda@hotmail.se>
2025-08-27 22:59:31 +0200

    edit img path

--- frontend/src/comp/blocks/Team.jsx
+++ frontend/src/comp/blocks/Team.jsx
@@ -4,7 +4,7 @@ const teamMembers = [
   {
     name: "Oskar",
     linkedin: "https://www.linkedin.com/",
-    src: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80",
+    src: "/oskar.webp",
   },
   {
     name: "Sofia",
@@ -27,10 +27,7 @@ const links = [
     label: "GitHub",
     href: "https://github.com/ssofiejohansson/project-final",
   },
-  {
-    label: "Link",
-    href: "#",
-  },
+
 ];
 
 export const Team = () => {
