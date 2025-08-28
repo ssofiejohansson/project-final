0e8515a9cc0f5e127080bc6e0915c09d214ebb0b
oskarnordin <oskarnordin1@gmail.com>
2025-08-28 15:56:40 +0200

    Lighter hover Btn
    Lighter hover Btn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,39 @@
-import { useEffect } from 'react';
-import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
-import EmailForm from './comp/EmailForm';
-import { Footer } from './comp/layout/Footer';
+import EmailForm from "./comp/EmailForm";
+import { Footer } from "./comp/layout/Footer";
 import { Loader } from "./comp/layout/Loader";
-import { Navbar } from './comp/layout/Navbar';
+import { Navbar } from "./comp/layout/Navbar";
 import { TopArrow } from "./comp/layout/TopArrow";
-import { About } from './pages/About';
-import { Admin } from './pages/Admin';
+import { About } from "./pages/About";
+import { Admin } from "./pages/Admin";
 import { Error } from "./pages/Error";
-import { Home } from './pages/Home';
-import { Login } from './pages/Login';
-import { Signup } from './pages/Signup';
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
 import useLoadingStore from "./stores/useLoadingStore";
 
 const ScrollToTop = () => {
-const location = useLocation();
-useEffect(() => {
-  window.scrollTo({ top: 0 });
-}, [location.pathname]);
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, [location.pathname]);
 
-return null; // this component only handles scrolling
+  return null; // this component only handles scrolling
 };
 
 export const App = () => {
   const loading = useLoadingStore((state) => state.loading);
 
   return (
-    <Router> 
-      <ScrollToTop/>     
+    <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen ">
         <Navbar />
         <main className="flex-grow mt-20 py-10">
