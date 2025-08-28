9f9388587f3db18bf64fb9edb2e392d13a87f189
Sofie <sofieannamatilda@hotmail.se>
2025-08-28 14:17:47 +0200

    change back to color orange

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = withMT({
   theme: {
     extend: {
       colors: {
-        main: "#000000",   // black
+        main: "#EF6E00",   // orange
         accent: "#ffd166", // yellow
         text: "#333333", // charcoal
         light: "#555555", // light-grey
